d78a7c723a3a41206bc0252f6820d8715f2c6b1a
Jia <jia@Zipcoders-MacBook-Pro-7.local>
2023-10-26 15:22:53 -0400

    finished

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 /**
  * Created by iyasuwatts on 10/17/17.
  */
@@ -5,5 +6,18 @@ public class Main {
 
     public static void main(String[] args ){
 
+        Scanner scanner1 = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+
+        String input = scanner1.next();
+
+
+        if(input.equalsIgnoreCase("Alice")) {
+            System.out.println("Greetings, " + input + "!");
+        } else if (input.equalsIgnoreCase("Bob")) {
+            System.out.println("Greetings, " + input + "!");
+        } else {
+            System.out.println("You are neither Alice nor Bob.");
+        }
     }
 }
